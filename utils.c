a0653dc8be7f2cedb8a956e18d9307be2aa0f9ba
guihocosta <guiihocosta@gmail.com>
2026-05-20 10:56:24 -0300

    feat(utils): simplificar cálculo de largura de texto UTF-8 e busca por prefixo

--- utils.c
+++ utils.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "utils.h"
 
 int is_prefixo(char *nome, const char *termo) {
@@ -7,10 +8,22 @@ int is_prefixo(char *nome, const char *termo) {
     
     int i = 0;
     while (termo[i] != '\0') {
-        if (nome[i] == '\0' || nome[i] != termo[i]) {
+        if (nome[i] == '\0' || tolower((unsigned char)nome[i]) != tolower((unsigned char)termo[i])) {
             return 0;
         }
         i++;
     }
     return 1;
+}
+
+int tamanho_texto(const char *s) {
+    if (s == NULL) return 0;
+    int tam = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        // Ignora bytes de continuação de caracteres UTF-8 (acentos)
+        if ((unsigned char)s[i] < 128 || (unsigned char)s[i] >= 192) {
+            tam++;
+        }
+    }
+    return tam;
 }
--- utils.h
+++ utils.h
@@ -4,4 +4,7 @@
 // Verifica se 'nome' começa com 'termo'
 int is_prefixo(char *nome, const char *termo);
 
+// Retorna a quantidade de caracteres visíveis da string (ajuste para UTF-8)
+int tamanho_texto(const char *s);
+
 #endif
