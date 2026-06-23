6f273789fad0fe957ce140fbc426761236dba615
guihocosta <guiihocosta@gmail.com>
2026-06-23 17:45:00 -0300

    feat(partida): atualizar a interface de partida com novos getters e setters

--- partida.h
+++ partida.h
@@ -13,8 +13,12 @@ Partida *partida_criar(int id, int time1_id, int time2_id, int gols1, int gols2)
 void partida_free(Partida *p);
 
 /* Funções para obter dados da partida (usadas) */
+int partida_get_id(Partida *p);
 int partida_get_time1(Partida *p);
 int partida_get_time2(Partida *p);
+int partida_get_gols1(Partida *p);
+int partida_get_gols2(Partida *p);
+void partida_set_placar(Partida *p, int g1, int g2);
 
 /* Imprime o placar da partida */
 void partida_imprimir(Partida *p, BDTimes *bdt, int largura_nome);
